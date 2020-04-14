62697bd2b98b9531b2cb001389aa33098b5b7af1
Hyun Woo Park <phu54321@naver.com>
2020-04-14 15:02:59 +0900

    :tada: initial commit
    lint-staged requires initial commit, so adding that later...

---
+++ jssrc/module.ts
@@ -0,0 +1,3 @@
+export function simpleFunction () {
+  alert('Test')
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+// webpack.config.js
+var path = require('path')
+
+module.exports = {
+  entry: {
+    main: './jssrc/index.ts'
+  },
+  output: {
+    filename: 'main.min.js',
+    path: path.resolve(__dirname, 'src'),
+    libraryTarget: 'window',
+    library: 'mainLib'
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader']
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      'node_modules'
+    ],
+    extensions: ['.js', '.ts']
+  }
+}
