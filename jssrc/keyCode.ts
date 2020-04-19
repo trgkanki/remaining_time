c58f674c24326996bb262ddaed751872d6d91958
Hyun Woo Park <phu54321@naver.com>
2020-04-19 14:30:19 +0900

    :wastebasket:  removed w/ac related code + example added
    this is template repo... not a full addon repo

--- jssrc/keyCode.ts
+++
@@ -1,5 +0,0 @@
-export const CTRL = 17
-export const DIGIT_0 = 49
-export const DIGIT_9 = 57
-export const ESC = 27
-export const TAB = 9
--- jssrc/window.d.ts
+++
@@ -1,3 +0,0 @@
-interface Window {
-  _wcInitialized: boolean | undefined
-}
