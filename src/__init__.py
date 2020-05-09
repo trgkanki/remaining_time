6698b1fe6812b486c4865b71ce4f8f6ace0998e9
Hyun Woo Park <phu54321@naver.com>
2020-05-09 17:42:25 +0900

    :art: using black .py formatter instead of autopep8
    Autopep8 won't handle specific formatting issues
    like using single quotes or double qotes...
    
    Black handles them nicely.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
 # addon_template v20.5.4i8
 #
@@ -14,19 +14,22 @@ from .jsBridge import evalJsExpr
 import os
 import json
 
+
 def readResource(filename):
     scriptDir = os.path.dirname(os.path.realpath(__file__))
     inputFilePath = os.path.join(scriptDir, filename)
-    with open(inputFilePath, 'r', encoding='utf-8') as f:
+    with open(inputFilePath, "r", encoding="utf-8") as f:
         return f.read()
 
+
 def onLoadNote(self, focusTo=None):
-    mainJs = readResource('main.min.js')
+    mainJs = readResource("main.min.js")
     self.web.eval(mainJs)
 
     def cb(data):
         askUser(json.dumps(data))
-    evalJsExpr(self.web, '1 + 1', cb)
+
+    evalJsExpr(self.web, "1 + 1", cb)
 
 
-Editor.loadNote = wrap(Editor.loadNote, onLoadNote, 'after')
+Editor.loadNote = wrap(Editor.loadNote, onLoadNote, "after")
--- src/jsBridge.py
+++ src/jsBridge.py
@@ -5,7 +5,8 @@ import re
 import uuid
 
 _handlerMap = {}
-_addonMessageRegex = re.compile(r'addonmsg:(\d+):(.+)')
+_addonMessageRegex = re.compile(r"addonmsg:(\d+):(.+)")
+
 
 def _onBridgeMessage(handled, message: str, context):
     matches = _addonMessageRegex.match(message)
@@ -19,13 +20,17 @@ def _onBridgeMessage(handled, message: str, context):
 
     return handled
 
+
 gui_hooks.webview_did_receive_js_message.append(_onBridgeMessage)
 
 
 def evalJsExpr(web, funcexpr, cb):
     handlerKey = str(uuid.uuid4().int)
     _handlerMap[handlerKey] = cb
-    web.eval('''
+    web.eval(
+        """
     Promise.resolve(%s).then(msg => {
         pycmd(`addonmsg:%s:${JSON.stringify(msg)}`)
-    })''' % (funcexpr, handlerKey))
+    })"""
+        % (funcexpr, handlerKey)
+    )
