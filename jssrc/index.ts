356f46b392b55372626cbe7790b178bc127368dd
Hyun Woo Park <phu54321@naver.com>
2020-06-20 22:08:51 +0900

    :bug: background css now applied by <style>
    it seems like settings css directly w/ jquery doesn't work. IDK why

--- jssrc/index.ts
+++ jssrc/index.ts
@@ -8,6 +8,17 @@ export function updateProgressBar (b64svg: string, progressBarMessage: string) {
     $barEl.attr('id', 'remainingTimeBar')
     $('body').append($barEl)
   }
-  $barEl.css('background', `url('data:image/svg+xml;base64,${b64svg}')`)
   $barEl.html(`${progressBarMessage} &nbsp; <a href=#resetRT onclick="pycmd('_rt_pgreset');return false;" title='Reset progress bar for this deck'>[⥻]</a>`)
+
+  let styleEl = document.getElementById('remainingTimeStylesheet')
+  if (!styleEl) {
+    styleEl = document.createElement('style')
+    styleEl.id = 'remainingTimeStylesheet'
+    document.head.appendChild(styleEl)
+  }
+  styleEl.textContent = `
+    #remainingTimeBar {
+      background: url('data:image/svg+xml;base64,${b64svg}')
+    }
+  `
 }
