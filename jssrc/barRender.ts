e9f4551b98b4df8e0aabf5c600eef878eac7879d
Hyun Woo Park <phu54321@naver.com>
2020-06-30 19:55:43 +0900

    :recycle: merged to updateProgressBar

--- jssrc/barRender.ts
+++ jssrc/barRender.ts
@@ -18,39 +18,7 @@ const goodColor = '114, 166, 249' // Good/Easy
 // TODO: support dark mode
 const backgroundColor = 'black'
 
-async function renderProgressBarSVG (estimator: Estimator) {
-  let timeSum = 0
-  for (const { dt } of estimator.logs) {
-    timeSum += dt
-  }
-
-  const currentRemainingReviews = await getRemainingReviews()
-  const elapsedTime = estimator.elapsedTime
-  const remainingTime = currentRemainingReviews / estimator.getSlope()
-  const progress = elapsedTime / (elapsedTime + remainingTime)
-  const pathSVGs: string[] = []
-  let rectX = 0
-
-  for (const log of estimator.logs) {
-    const rectW = log.dt / timeSum * progress
-    const rectAlpha =
-     (log.dt < clampMinTime) ? maxAlpha
-       : (log.dt > clampMaxTime) ? minAlpha / 2
-         : maxAlpha - (log.dt - clampMinTime) / (clampMaxTime - clampMinTime) * (maxAlpha - minAlpha)
-    const rectColor = (log.ease === 1) ? againColor : goodColor
-    pathSVGs.push(`'<path d="M${rectX} 0 h${rectW} V1 h-${rectW} Z" fill="rgba(${rectColor}, ${rectAlpha})" shape-rendering="crispEdges" />`)
-    rectX += rectW
-  }
-
-  return `
-  <svg width="1" height="1" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0 0 h1 V1 h-1 Z" fill="${backgroundColor}" />
-      ${pathSVGs.join('')}
-  </svg>
-  `
-}
-
-export function renderProgressBar (b64svg: string, progressBarMessage: string) {
+function updateDOM (b64svg: string, progressBarMessage: string) {
   let $barEl = $('#remainingTimeBar')
   if ($barEl.length === 0) {
     $barEl = $('<div></div>')
@@ -73,7 +41,7 @@ export function renderProgressBar (b64svg: string, progressBarMessage: string) {
   `
 }
 
-export async function renderBar () {
+export async function updateProgressBar () {
   const currentRemainingReviews = await getRemainingReviews()
   const remainingLoad = reviewLoad(currentRemainingReviews)
   if (remainingLoad === 0) return
@@ -83,7 +51,31 @@ export async function renderBar () {
   const remainingTime = remainingLoad / estimator.getSlope()
   const message = `Elapsed ${t2s(elapsedTime)},  Remaining ${t2s(remainingTime)}, Total ${t2s(elapsedTime + remainingTime)}`
 
-  const svgContent = await renderProgressBarSVG(estimator)
-  const b64svg = Base64.encode(svgContent)
-  renderProgressBar(b64svg, message)
+  const progress = elapsedTime / (elapsedTime + remainingTime)
+  const pathSVGs: string[] = []
+  let rectX = 0
+
+  let timeSum = 0
+  for (const { dt } of estimator.logs) {
+    timeSum += dt
+  }
+
+  for (const log of estimator.logs) {
+    const rectW = log.dt / timeSum * progress
+    const rectAlpha =
+     (log.dt < clampMinTime) ? maxAlpha
+       : (log.dt > clampMaxTime) ? minAlpha / 2
+         : maxAlpha - (log.dt - clampMinTime) / (clampMaxTime - clampMinTime) * (maxAlpha - minAlpha)
+    const rectColor = (log.ease === 1) ? againColor : goodColor
+    pathSVGs.push(`<path d="M${rectX} 0 h${rectW} V1 h-${rectW} Z" fill="rgba(${rectColor}, ${rectAlpha})" shape-rendering="crispEdges" />`)
+    rectX += rectW
+  }
+
+  const b64svg = Base64.encode(`
+  <svg width="1" height="1" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0 h1 V1 h-1 Z" fill="${backgroundColor}" />
+      ${pathSVGs.join('')}
+  </svg>
+  `)
+  updateDOM(b64svg, message)
 }
