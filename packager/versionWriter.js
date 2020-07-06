3bb3b87525242277c6a9d57a2b1722964f1f0532
Hyun Woo Park <phu54321@naver.com>
2020-07-06 19:21:14 +0900

    :sparkles: update package-lock.js version field on dist

--- packager/versionWriter.js
+++ packager/versionWriter.js
@@ -7,6 +7,7 @@ exports.updateFilesVersionString = async function (newVersion, changelogMessage)
   console.log(`Updating to "${repoName} v${newVersion}"`)
 
   shelljs.sed('-i', /"version": "(.+?)"/, `"version": "${newVersion}"`, 'package.json')
+  shelljs.sed('-i', /^ {2}"version": "(.+?)"/, `  "version": "${newVersion}"`, 'package-lock.json')
   shelljs.sed('-i', /^# .+v(\d+)\.(\d+)\.(\d+)\.(\d+)$/m, `# ${repoName} v${newVersion}`, 'src/__init__.py')
   fs.writeFileSync('src/VERSION', newVersion)
 }
