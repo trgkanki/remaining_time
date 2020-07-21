834fe3e6cae5890b7929e2445d4272cbf92f0d26
Hyun Woo Park <phu54321@naver.com>
2020-07-21 11:58:01 +0900

    :rotating_light: removed typescript-related things from dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    // disable the rule for all files
-        "@typescript-eslint/explicit-function-return-type": "off"
-    },
-    "overrides": [
-    {
-        // enable the rule specifically for TypeScript files
-        "files": ["*.ts", "*.tsx"],
-        "rules": {
-        "@typescript-eslint/explicit-function-return-type": ["error"]
-        }
-    }
-]};
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  }
+}
