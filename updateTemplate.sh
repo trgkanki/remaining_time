f2f7b4c42d9bfa93b4d736fef788395d47c9e941
Hyun Woo Park <phu54321@naver.com>
2020-07-24 23:57:06 +0900

    :sparkles: implied upstream branch for updateTemplate.sh
    upstream branch is stored in BASEBRANCH file, and updateTemplate uses
    the file as a incoming branch.

--- updateTemplate.sh
+++ updateTemplate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function usage {
-  echo ' $ ./updateTemplate.sh base         # merge from template/base'
+  echo ' $ ./updateTemplate.sh [branch=`cat BASEBRANCH`]          # merge from template/(original base)'
+  echo '   # To change the upstream branch, modify BASEBRANCH file'
   echo '   < Fix your conflicts if necessaty, git commit >'
   echo ' $ git commit'
 }
@@ -11,16 +12,38 @@ function merge_message {
   echo ":twisted_rightwards_arrows: merge from template/$1 ($dateString)"
 }
 
-if [[ `git status` == *"nothing to commit, working tree clean"* ]]; then
+# if [[ `git status` == *"nothing to commit, working tree clean"* ]]; then
+if true; then
+  upstreamBranch=`cat BASEBRANCH 2> /dev/null`
+  upstreamBranch="$(sed -e 's/[[:space:]]*$//' <<<${upstreamBranch})"  # Strip whitespace
+  if ! [ -z "$1" ]; then
+    requiredUpstreamBranch=$1
+  fi
+
+  if (
+    [ -z "$upstreamBranch" ] ||
+    ( [ ! -z "$requiredUpstreamBranch" ] && [ "$requiredUpstreamBranch" != "$upstreamBranch" ])
+  ); then
+    if [ -z "$requiredUpstreamBranch" ]; then
+      echo "Please specify incoming branch. (BASEBRANCH file not exists)"
+      exit
+    fi
+
+    echo $requiredUpstreamBranch > BASEBRANCH
+    echo Commit BASEBRANCH file and re-execute binary without branch arguments.
+    echo " $ $0"
+    exit
+  fi
+
   # Try merging
   git fetch --all
-  if [ -z `git branch -r | grep " template/$1$"` ]; then
-    echo 'Remote branch "template/$1" not found.'
+  if [ -z `git branch -r | grep " template/$upstreamBranch"` ]; then
+    echo "Remote branch template/$upstreamBranch not found."
     usage
     exit
   fi
-  git merge template/$1 --squash
-  sed -i "s#Squashed commit of the following:#$(merge_message $1)#" .git/SQUASH_MSG
+  git merge template/$upstreamBranch --squash
+  sed -i "s#Squashed commit of the following:#$(merge_message $upstreamBranch)#" .git/SQUASH_MSG
 else
   echo Error: Branch must be clean before merge.
   usage
