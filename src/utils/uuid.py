501dcb4dc93dbc046ca93c1f7e5f54e2a975de64
Hyun Woo Park <phu54321@naver.com>
2020-08-14 16:54:25 +0900

    :sparkles: warn when using addon_template's UUID

--- src/utils/uuid.py
+++ src/utils/uuid.py
@@ -1,4 +1,5 @@
 from .resource import readResource
+from .configrw import getCurrentAddonName
 import aqt
 
 _uuid = None
@@ -8,13 +9,19 @@ def addonUUID():
     global _uuid
     if _uuid is None:
         _uuid = readResource("UUID").strip()
+
     return _uuid
 
 
 def dupUUIDCheck():
     """ Check for duplicate UUID and raise exception if one exists """
-
     uuid = addonUUID()
+
+    if (
+        uuid == "aceb54e9-1323-49aa-9644-cf0a28a3d0c3"
+        and getCurrentAddonName() != "addon_template"
+    ):
+        raise RuntimeError("Assign new UUID to addon %s" % getCurrentAddonName())
 
     if not hasattr(aqt, "_uuidDict"):
         aqt._uuidDict = {}
