4a83940acecc39aa8658f5dd3f557aac8721da3b
Hyun Woo Park <phu54321@naver.com>
2020-08-14 16:25:32 +0900

    :sparkles: modify "addon_template" literals on initFromTemplates

--- initFromTemplate.sh
+++ initFromTemplate.sh
@@ -26,5 +26,12 @@ git commit --allow-empty -m ':tada: initial empty commit (for merge head)'
 git merge template/$2 --squash --allow-unrelated-histories
 sed -i "s#Squashed commit of the following:#$(merge_message $2)#" .git/SQUASH_MSG
 git commit --no-edit
+sed -i "s/\"name\": \"addon_template\",/\"name\": \"$1\",/" package.json
+sed -i "s/\"name\": \"addon_template\",/\"name\": \"$1\",/" package-lock.json
+sed -i "s/# addon_template v/# $1 v/" src/__init__.py
 npm i
+git add .
+git commit -m ":tata: generated from template/$2"
 echo 'Project generated from template'
+
+# .* v(\d*\.\d*\.\d*[i.]\d*)$
