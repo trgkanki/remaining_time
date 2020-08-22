502bd70bb18d5709d99a34f33cb78d23dbe54ac4
Hyun Woo Park <phu54321@naver.com>
2020-08-22 19:34:32 +0900

    ✅ test for notify() propagation upward

--- src/tests/test_observable/test_object.py
+++ src/tests/test_observable/test_object.py
@@ -7,6 +7,11 @@ class TestClass:
         self.attr2 = 2
 
 
+class TestClass2:
+    def __init__(self):
+        self.t = TestClass()
+
+
 notified = set()
 
 
@@ -19,10 +24,27 @@ def notifyLogger(v):
 
 def test_object_notification():
     a = observable(TestClass())
-    notified.clear()
 
+    notified.clear()
     a.registerObserver(notifyLogger(a))
     assert len(notified) == 0
+
     a.attr1 = 2
     assert notified == {a}
     assert a.attr1 == 2  # properly changed?
+
+
+def test_nested_object_notification():
+    a = observable(TestClass2())
+
+    notified.clear()
+    a.registerObserver(notifyLogger(a))
+    a.t.registerObserver(notifyLogger(a.t))
+    assert len(notified) == 0
+
+    oldAT = a.t
+
+    a.t.attr1 = 2
+    assert notified == {a, a.t}
+    assert a.t.attr1 == 2  # properly changed?
+    assert oldAT == a.t
