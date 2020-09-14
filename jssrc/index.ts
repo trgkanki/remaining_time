ca8d857749ef9d5efb87a0a6eecf21b3b21e58e3
Hyun Woo Park <phu54321@naver.com>
2020-09-14 22:01:39 +0900

    ⚡️ prevent duplicate import of css/js

--- jssrc/index.ts
+++ jssrc/index.ts
@@ -1,31 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/camelcase */
-
-import { updateEstimator } from './updater'
-import { updateProgressBar } from './barRender'
-
-async function main () {
-  // ignore running on answer side.
-  // updateEstimator thinks the user got the question wrong if new/lrn/rev
-  // haven't changed after the last updateEstimator() call. (which can happen
-  // if you got the learning card wrong) So if updateEstimator gets called
-  // both on question side and answer side, the time you spent thinking
-  // the question will be regarded as 'wrong', polluting progress bar graphics
-  //
-  // Also, on AnkiDroid #qa has 'question' and 'answer' class name, but on desktop
-  // #qa has neither of them. Hence we check the absence of .answer. On desktop
-  // main() will be called only on question side anyway.
-
-  const qaEl = document.getElementById('qa')
-  if (qaEl && !qaEl.classList.contains('answer')) {
-    await updateEstimator()
-  }
-  await updateProgressBar()
-}
 
 (function () {
   const windowAny = window as any
   if (!windowAny.__rtt_initialized) {
     windowAny.__rtt_initialized = true
+
+    // Import needed modules here, as importing them outside duplicate import check
+    // might multiple-import css or event handlers.
+    const { updateEstimator } = require('./updater')
+    const { updateProgressBar } = require('./barRender')
+
+    // eslint-disable-next-line no-inner-declarations
+    async function main () {
+      // ignore running on answer side.
+      // updateEstimator thinks the user got the question wrong if new/lrn/rev
+      // haven't changed after the last updateEstimator() call. (which can happen
+      // if you got the learning card wrong) So if updateEstimator gets called
+      // both on question side and answer side, the time you spent thinking
+      // the question will be regarded as 'wrong', polluting progress bar graphics
+      //
+      // Also, on AnkiDroid #qa has 'question' and 'answer' class name, but on desktop
+      // #qa has neither of them. Hence we check the absence of .answer. On desktop
+      // main() will be called only on question side anyway.
+
+      const qaEl = document.getElementById('qa')
+      if (qaEl && !qaEl.classList.contains('answer')) {
+        await updateEstimator()
+      }
+      await updateProgressBar()
+    }
 
     // AnkiDroid: to main() for each card review, we hook onPageFinished function
     if (windowAny.onPageFinished) {
