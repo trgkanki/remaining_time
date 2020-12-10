64ec5d76e0487392318eac6799d117e872e8a8ea
박현우 <phu54321@naver.com>
2020-12-10 19:22:52 +0900

    refactor: moved splitCookieSave to ankiLocalStorage impl
    since it may also help storing other local data
    
    and this is cookie-specific issue, not an limitation of LocalStorage API.

--- jssrc/utils/ankiLocalStorage.ts
+++ jssrc/utils/ankiLocalStorage.ts
@@ -9,17 +9,51 @@ import isMobile from 'is-mobile'
 import Cookies from 'js-cookie'
 import { callPyFunc } from './pyfunc'
 
+/**
+ * Split payload to smaller chunks for compatibility w/ ankiDroid, which has
+ * 6kb cookie limit per cookie value.
+ *
+ * @param key LocalStorage key to save
+ * @param payload Payload to save
+ */
+function splitCookieSave (key: string, payload: string) {
+  const packetSize = 4096
+  let packetIndex = 0
+  for (let i = 0; i < payload.length; i += packetSize) {
+    const packet = payload.slice(i, i + packetSize)
+    Cookies.set(`${key}_${packetIndex}`, packet)
+    packetIndex++
+  }
+  Cookies.set(`${key}_${packetIndex}`, '')
+}
+
+/**
+ * Load things saved with splitSave.
+ *
+ * @param key LocalStorage key to save
+ * @returns Concatenated payload. If payload hasn't been saved, return empty string.
+ */
+function splitCookieLoad (key: string): string {
+  const chunks: string[] = []
+  for (let packetIndex = 0; ; packetIndex++) {
+    const chunk = Cookies.get(`${key}_${packetIndex}`)
+    if (!chunk) break
+    chunks.push(chunk)
+  }
+  return chunks.join('')
+}
+
 export default {
   async setItem (key: string, data: string) {
     if (isMobile()) {
-      Cookies.set(key, data)
+      splitCookieSave(key, data)
     } else {
       await callPyFunc('localStorageSetItem', key, data)
     }
   },
   async getItem (key: string): Promise<string | null> {
     if (isMobile()) {
-      return Cookies.get(key) || null
+      return splitCookieLoad(key) || null
     } else {
       return callPyFunc('localStorageGetItem', key)
     }
