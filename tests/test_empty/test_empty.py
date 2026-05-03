45deaa8c8471836895dda369da6bfd9002bcf08d
Park Hyunwoo <phu54321@naver.com>
2026-05-03 13:34:13 +0900

    chore!: remove obsolete qdlg
    It is not maintained: better to use pysimpleguiqt or other libraries.

--- tests/test_empty/test_empty.py
+++ tests/test_empty/test_empty.py
@@ -13,31 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import threading
-
-local = threading.local()
+# This is just to make pytest happy with 0 tests running.
 
 
-def getQDlgStack():
-    try:
-        return local.qDlgStack
-    except AttributeError:
-        local.qDlgStack = []
-        return local.qDlgStack
-
-
-def pushQDlgStack(el):
-    getQDlgStack().append(el)
-
-
-def popQDlgStack(el):
-    lastEl = getQDlgStack().pop()
-    assert lastEl == el
-
-
-def qDlgStackTop():
-    return getQDlgStack()[-1]
-
-
-def qDlgStackGetDialog():
-    return getQDlgStack()[0]
+def test_empty():
+    pass
